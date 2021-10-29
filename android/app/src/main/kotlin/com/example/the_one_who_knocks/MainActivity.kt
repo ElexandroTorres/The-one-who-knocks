c870084439e92614477896ef2cbaa5296e2ec281
Elexandro Torres <elexandro.torres@gmail.com>
2021-10-29 11:24:39 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/the_one_who_knocks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.the_one_who_knocks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          //backgroundColor: const Color(0xFF1d584b),
+          title: const Text(
+            'I\'m the one who knocks',
+            style: TextStyle(color: Color(0xFFccffff)),
+          ),
+        ),
+      ),
+    ),
+  );
+}
