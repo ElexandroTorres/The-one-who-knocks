c20c0568b1940188aae82f6fedeffb0d286a1d86
Elexandro Torres <elexandro.torres@gmail.com>
2021-10-29 12:06:59 -0300

    Some changes, simple image and text included

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,23 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: const Color(0xFF1d584b),
         appBar: AppBar(
-          //backgroundColor: const Color(0xFF1d584b),
+          backgroundColor: const Color(0xFF72c89b),
           title: const Text(
             'I\'m the one who knocks',
             style: TextStyle(color: Color(0xFFccffff)),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: const <Widget>[
+              Image(
+                image: AssetImage('images/heisenberg.png'),
+              ),
+              Text('I\'m not in danger'),
+              Text("I'\M THE DANGER!!!"),
+            ],
           ),
         ),
       ),
