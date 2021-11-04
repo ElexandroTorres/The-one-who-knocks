01d4bb218706e22fe19768fe4202481850c773bd
Elexandro Torres <elexandro.torres@gmail.com>
2021-11-04 12:15:22 -0300

    simple app complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,52 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFF1d584b),
-        appBar: AppBar(
-          backgroundColor: const Color(0xFF72c89b),
-          title: const Text(
-            'I\'m the one who knocks',
-            style: TextStyle(color: Color(0xFFccffff)),
-          ),
-        ),
-        body: Center(
-          child: Column(
-            children: const <Widget>[
-              Image(
-                image: AssetImage('images/heisenberg.png'),
-              ),
-              Text('I\'m not in danger'),
-              Text("I'\M THE DANGER!!!"),
-            ],
+      title: 'I am the danger',
+      home: Home(),
+    ),
+  );
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF1d584b),
+      appBar: AppBar(
+        backgroundColor: Colors.black54,
+        title: const Text(
+          'I am the one who knocks',
+          style: TextStyle(
+            color: Colors.white24,
+            fontFamily: 'BBad',
+            fontSize: 32,
           ),
         ),
       ),
-    ),
-  );
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const <Widget>[
+            Image(
+              image: AssetImage('images/heisenberg.png'),
+            ),
+            Text(
+              'I\'m not in danger\nI AM THE DANGER!!!',
+              style: TextStyle(
+                color: Colors.black45,
+                fontFamily: 'Fruktur',
+                fontSize: 24,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
